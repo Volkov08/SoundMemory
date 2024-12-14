37d0bed79a01824789967cf0f05ecc681aa3b7a6
Volkov08 <80154311+Volkov08@users.noreply.github.com>
2024-12-14 01:09:17 +0100

    barebones game struct

---
+++ main.js
@@ -0,0 +1,314 @@
+var fileNames = null;
+
+async function loadFileNames() {
+    indexFile = "sounds/index.json";
+    try {
+        const response = await fetch(indexFile);
+        if (!response.ok) {
+            throw new Error("file load failed");
+        }
+        const data = await response.json();
+        fileNames = data;
+
+        startGame();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const gameArea = document.getElementById("gameArea");
+const audioContext = new AudioContext();
+audioContext.suspend();
+const gainNode = audioContext.createGain();
+gainNode.connect(audioContext.destination);
+
+const volumeSlider = document.getElementById("volume");
+volumeSlider.oninput = function () {
+    gameSettings.volume = this.value / 100;
+    gainNode.gain.value = gameSettings.volume;
+};
+
+var cards = [];
+var audioCollection = [];
+var audioLookup = []; //assigns card to audio
+
+//var tracks = [];
+var pauseTimeouts = [];
+var playedInTurn = [];
+var cardA = null,
+    cardB = null;
+playing = false;
+endTurnOnStop = false;
+
+var solvedPairs = [];
+var scores = [0, 0];
+var turn = 0;
+
+var gameSettings = {
+    cards: 40, //even, at most fileNames.length * 2
+    players: 1,
+    duration: 5,
+    volume: 1,
+    maxPlays: 1,
+};
+
+//
+//
+// ---------------------
+// setup
+// ---------------------
+//
+//
+
+function startGame() {
+    gameSettings.cards = Math.min(gameSettings.cards, fileNames.length * 2);
+    alignCards();
+    console.log(fileNames);
+
+    let selection = Array.from({ length: gameSettings.cards }, (_, i) => i);
+
+    for (let i = 0; i < gameSettings.cards / 2; i++) {
+        let audio = new Audio(`sounds/${fileNames[selection[i]]}`);
+        let track = audioContext.createMediaElementSource(audio);
+        track.connect(gainNode);
+        audioCollection.push(audio);
+        //tracks.push(track);
+    }
+
+    audioLookup = [];
+    for (let i = 0; i < gameSettings.cards; i++) {
+        let card = createCard(i);
+
+        cards.push(card);
+        gameArea.appendChild(card);
+
+        audioLookup.push(i % (gameSettings.cards / 2));
+    }
+
+    //Fisher-Yates shuffle
+    for (let i = 0; i < gameSettings.cards; i++) {
+        let j = randInt(gameSettings.cards - i) + i;
+        let temp = audioLookup[i];
+        audioLookup[i] = audioLookup[j];
+        audioLookup[j] = temp;
+    }
+
+    document.getElementById("endTurn").onclick = () => {
+        if (cardA != null && cardB != null) {
+            //only end when two cards were uncovered
+
+            if (!playing) endTurn();
+            else endTurnOnStop = true;
+        }
+    };
+
+    volumeSlider.oninput();
+}
+
+function createCard(i) {
+    let card = document.createElement("button");
+    card.classList.add("card");
+    card.onclick = function () {
+        clickHandler(i);
+    };
+    return card;
+}
+
+//
+//
+// ---------------------
+//  game logic
+// ---------------------
+//
+//
+
+function endTurn() {
+    cards.forEach((card) => {
+        card.classList.remove("uncovered");
+    });
+    playedInTurn = [];
+    cardA = null;
+    cardB = null;
+    turn++;
+    turn %= gameSettings.players;
+
+    endTurnOnStop = false;
+
+    console.log(
+        `Turn finished, current player:${turn}, score: ${scores[0]}|${scores[1]}`
+    );
+}
+
+function matchFound() {
+    cards[cardA].classList.add("found");
+    cards[cardB].classList.add("found");
+
+    solvedPairs[cardA] = true;
+    solvedPairs[cardB] = true;
+
+    scores[turn]++;
+    endTurnOnStop = true;
+}
+
+function clickHandler(i) {
+    if (audioContext.state === "suspended") audioContext.resume();
+
+    let j = audioLookup[i];
+
+    if (!audioCollection[j].paused) return true;
+
+    if (!audioCollection[j].HAVE_ENOUGH_DATA) return false;
+
+    if (!uncoverCard(i)) return false;
+
+    playCard(i);
+
+    pauseTimeouts[i] = setTimeout(() => {
+        pauseCard(i);
+    }, gameSettings.duration * 1000);
+    return true;
+}
+
+//
+//
+// ---------------------
+//  card utilities
+// ---------------------
+//
+//
+
+function pauseAll() {
+    audioCollection.forEach((audio, i) => {
+        pauseCard(i, true);
+        clearTimeout(pauseTimeouts[i]);
+        pauseTimeouts[i] = null;
+    });
+    playing = false;
+}
+
+function uncoverCard(i) {
+    let card = cards[i];
+
+    if (solvedPairs[i]) return false; //dont allow solved pairs to be uncovered again
+    //dont allow new cards to be uncovered, but old ones can be played
+    if (!playedInTurn[i] && cardA != null && cardB != null) return false;
+
+    if (playedInTurn[i] == undefined) {
+        console.log("uncovered", i);
+
+        playedInTurn[i] = 1;
+        if (cardA == null) cardA = i;
+        else if (cardA != i) cardB = i;
+
+        card.classList.add("uncovered");
+    } else if (playedInTurn[i] < gameSettings.maxPlays) {
+        playedInTurn[i]++;
+    } else {
+        return false;
+    }
+    card.innerText = `${playedInTurn[i]}/ ${gameSettings.maxPlays}`;
+    console.log(cardA, cardB);
+    if (cardA != null && cardB != null) {
+        if (audioLookup[cardA] == audioLookup[cardB]) matchFound();
+        else if (
+            Math.min(playedInTurn[cardA], playedInTurn[cardB]) ==
+            gameSettings.maxPlays
+        ) {
+            endTurnOnStop = true;
+        }
+    }
+    return true;
+}
+
+function playCard(i) {
+    pauseAll();
+
+    let card = cards[i];
+
+    let j = audioLookup[i];
+
+    console.log(`playing ${i}, audio ${j}`);
+
+    audioCollection[j].play();
+    card.classList.add("playing");
+
+    playing = true;
+}
+
+function pauseCard(i, force = false) {
+    let card = cards[i];
+
+    //console.log("stopped", i);
+
+    let j = audioLookup[i];
+    audioCollection[j].pause();
+    audioCollection[j].currentTime = 0; // rewind to start
+    card.classList.remove("playing");
+
+    playing = false;
+
+    //only end turn if playback finished, not if stopped by starting new playback
+    if (endTurnOnStop && !force) endTurn();
+}
+
+//
+//
+// ---------------------
+//  alignment logic
+// ---------------------
+//
+//
+
+function alignCards() {
+    let gAHeight = gameArea.clientHeight;
+    let gAWidth = gameArea.clientWidth;
+
+    let compStyle = getComputedStyle(gameArea);
+
+    paddig = parseInt(compStyle.padding);
+    margin = parseInt(compStyle.getPropertyValue("--card-margin"));
+
+    availWidth = gAWidth - 2 * paddig;
+    availHeight = gAHeight - 2 * paddig;
+
+    let ratio = availHeight / availWidth;
+    let { x, y } = closestIntegerRatio(ratio, gameSettings.cards);
+    let size = Math.floor(
+        Math.min(availWidth / x, availHeight / y) - 2 * margin
+    );
+
+    console.log(`aligning cards to ${x}x${y} grid, size: ${size}`);
+
+    if (y > x) gameArea.style.justifyContent = "center";
+    else gameArea.style.justifyContent = "left";
+    gameArea.style.setProperty("--card-size", `${size}px`);
+}
+
+const closestIntegerRatio = (r, n) => {
+    let a = 0;
+    let b = 0;
+    let flip = false;
+    if (r > 1) {
+        r = 1 / r;
+        flip = true;
+    }
+    while (a * b < n) {
+        a++;
+        b = Math.floor(a * r);
+    }
+    if (flip) return { x: b, y: a };
+    return { x: a, y: b };
+};
+
+const randInt = (n) => Math.floor(Math.random() * n);
+
+//
+//
+// ---------------------
+//  start game
+// ---------------------
+//
+//
+
+window.onresize = alignCards;
+loadFileNames();
--- process.py
+++ process.py
@@ -1,20 +1,68 @@
 import os
-import soundfile as sf
+import pydub as pd
 
-import matplotlib.pyplot as plt
-import numpy as np
-
+#this directory is empty in the repo since i does not make sense to store the entire (sometimes large) audio files in the repo
 inputDir = "rawFiles"
 outputDir = "sounds" 
 
-rawFiles = os.listdir(rawFiles)
+rawFiles = os.listdir(inputDir)
+segmentDuration = 5000
+sweepDuration = 1000
 
+def clearDir(dir):
+    for file in os.listdir(dir):
+        os.remove(dir + "/" + file)
+
+
+#cut audio files into segments without silence
+def cut(audio):
+    if audio.duration_seconds < segmentDuration/1000:
+        return None
+    loudest = audio[:-segmentDuration].max_dBFS
+    validSegment = None
+    for i in range(int((len(audio)-segmentDuration)/sweepDuration)):
+        segment = audio[i*sweepDuration:(i+1)*sweepDuration]
+        if segment.max_dBFS >= loudest-3:
+            validSegment = audio[i*sweepDuration:i*sweepDuration + segmentDuration]
+            break
+    if validSegment is None:
+        return None
+    normalized = pd.effects.normalize(validSegment)
+    faded = normalized.fade_in(500).fade_out(500)
+    return faded
+
+def genFilename(file,i):
+    return outputDir + "/" + str(i) +"_" + file.replace(" ","").replace("-","_")
+
+clearDir(outputDir)
+
+i = 0
 for file in rawFiles:
-    audio, samplerate = sf.read(inputDir + "/" + file)
-    cut(audio, samplerate)
+    audio = pd.AudioSegment.from_mp3(inputDir + "/" + file)
+    if not "00" in file:
+        pass
+        #continue
+    cutAudio = cut(audio)
+    if cutAudio is None:
+        print("No valid segment found in " + file)
+        continue
+    cutAudio.export(genFilename(file,i), format="mp3")
+    i += 1
 
-def cut(audio, samplerate):
-    time = np.arange(0, len(audio)/samplerate, 1/samplerate)
-    plt.plot(time, audio)
-    plt.show()
-    
+#generate index file
+index = open(outputDir+"/index.json", "w")
+index.write("[")
+
+#comma before each filename except the first for valid json (smh no trailing commas)
+comma = False
+for file in os.listdir(outputDir):
+    if not file.endswith(".mp3"):
+        continue
+    if not comma:
+        comma = True
+    else:
+        index.write(",")
+    index.write('\n"' + file + '"')
+
+index.write("\n]")
+index.close()
